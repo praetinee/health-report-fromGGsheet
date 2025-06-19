a399bea2c1212dbab85017a772f12be518efbd1c
praetinee <praetinee@gmail.com>
2025-06-19 14:26:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1137,7 +1137,7 @@ if "person" in st.session_state:
         st.markdown(render_section_header("ผลการตรวจไวรัสตับอักเสบเอ (Viral hepatitis A)"), unsafe_allow_html=True)
         st.markdown(f"""
         <div style="text-align: left; font-size: 18px; margin: 1rem 0;">
-        {hep_a_result}
+        {hep_a_raw}
         </div>
         """, unsafe_allow_html=True)
         
